cb3fc798ad26d66d32dcea02bfc39e1dde579b0a
Quoc Dat <ngoluuquocdat@gmail.com>
2022-06-10 16:35:28 +0700

    User chat box in Provider View Page

--- src/Components/TourProviderPage.js
+++ src/Components/TourProviderPage.js
@@ -2,11 +2,13 @@ import React from 'react'
 import HeaderNav from './Header/HeaderNav';
 import TopTours from './HomePage/TopTours';
 import TourCard from './TourCard';
+import UserChatBox from './UserChatBox';
 import axios from "axios";
 import ReactLoading from "react-loading";
 import { connect } from "react-redux";
 import { withRouter } from 'react-router-dom'
 import { Pagination } from "@mui/material";
+import { toast } from 'react-toastify';
 import { FaCaretDown } from 'react-icons/fa';
 import { FiStar } from 'react-icons/fi'
 import { HiOutlineMail, HiOutlinePhone } from 'react-icons/hi'
@@ -26,7 +28,8 @@ class ProviderPage extends React.Component {
         sort: 'latest',
         isShowSortMenu: false,
         isLoadingProvider: false,
-        isLoadingAvailable: false
+        isLoadingAvailable: false,
+        isOpenChatBox: false   
     }
 
     baseUrl = this.props.reduxData.baseUrl;
@@ -47,14 +50,8 @@ class ProviderPage extends React.Component {
             });
         } catch (error) {
             if (!error.response) {
-            // fake api response
-            // fake api response
-            const resProvider = provider;
-            // set state
-            this.setState({
-                provider: resProvider,
-            });
-            return;
+                toast.error('Network error')
+                return;
             }
             if (error.response.status === 404) {
                 console.log(error);
@@ -89,6 +86,12 @@ class ProviderPage extends React.Component {
             // call api to get tours and set state
             this.fetchDataTour(page, perPage, sort)
         }
+        if(prevProps.reduxData.user !== this.props.reduxData.user){
+            if(this.props.reduxData.user === null) {
+                window.location.reload();
+                return;
+            }
+        }
     }
 
     async fetchDataTour(page, perPage, sort) {
@@ -103,13 +106,8 @@ class ProviderPage extends React.Component {
             });
         } catch (error) {
             if (!error.response) {
-            // fake api response
-            const resTours = listTours;
-            // set state
-            this.setState({
-                tours: resTours,
-            });
-            return;
+                toast.error('Network error')
+                return;
             }
             if (error.response.status === 404) {
                 console.log(error);
@@ -154,7 +152,7 @@ class ProviderPage extends React.Component {
         const { tours, provider, totalCount, page, totalPage } = this.state;
         const isToursEmpty = provider.tourAvailable === 0;
         const isLoadingProvider = this.state.isLoadingProvider;
-        const { sort, isShowSortMenu } = this.state;
+        const { sort, isShowSortMenu, isOpenChatBox } = this.state;
         const url = `url('${this.baseUrl+provider.avatarUrl}')`;
 
         return (
@@ -232,6 +230,7 @@ class ProviderPage extends React.Component {
                                 <span>Description:</span>
                                 <p>{provider.description}</p>
                             </div> 
+                            <button className='open-chat-btn' onClick={() => this.setState({isOpenChatBox: !this.state.isOpenChatBox})}>CHAT</button>
                         </div>
                     </div>
                     <hr className="section-divide-hr"></hr>
@@ -324,259 +323,22 @@ class ProviderPage extends React.Component {
                             </div>
                         </>
                     }
+                    {
+                        isOpenChatBox &&
+                        <div className='user-chat-box-container'>
+                            <UserChatBox 
+                                providerId={provider.id} 
+                                providerName={provider.name}
+                                providerAvatar={provider.avatarUrl}
+                                closeChatBox={() => this.setState({isOpenChatBox: false})}
+                            />  
+                        </div>        
+                    }
                 </div>
             </div>
         )
     }
 }
-
-const provider = {
-    id: 1,
-    name: 'Hoi An Express',
-    phone: '0905123456',
-    email: 'info@hoianexpress.com.vn',
-    address: '32 Tien Giang St, Tan Binh, Ho Chi Minh City, Viet Nam',
-    dateCreated: '28/03/2022',
-    averageRating: 4.4,
-    tourAvailable: 180,
-    description: '',
-    avatarUrl: 'https://pbs.twimg.com/profile_images/721952678016737280/ppDehV3R_400x400.jpg'
-}
-
-const listTours = [
-    {
-      id: 1,
-      tourName: "FULL-DAY HAI VAN PASS & LANG CO BEACH & IN HUE CITY",
-      reviews: 10,
-      rating: 4.8,
-      viewCount: 10,
-      isPrivate: false,
-      minPrice: 384,
-      duration: 1,
-      route: "london",
-      description: "For everything hunky-dory",
-      thumbnailPath:
-        "https://hoianexpress.com.vn/wp-content/uploads/2019/12/LANG-CO-BEACH-10.jpg",
-    },
-    {
-      id: 2,
-      tourName:
-        "HALF-DAY FOODIE TOUR BY BICYCLE & VISIT TRA QUE VEGETABLE VILLAGE",
-      reviews: 10,
-      rating: 4.4,
-      viewCount: 10,
-      isPrivate: false,
-      minPrice: 45,
-      duration: 0.5,
-      route: "las-vegas",
-      description: "An offer you can't refuse",
-      thumbnailPath:
-        "https://hoianexpress.com.vn/wp-content/uploads/2019/12/Foodie_11-680x500.jpg",
-    },
-    {
-      id: 3,
-      tourName: "HOI AN MYSTERIOUS NIGHT TOUR WITH DINNER FROM DA NANG",
-      reviews: 10,
-      rating: 3.8,
-      viewCount: 10,
-      isPrivate: true,
-      minPrice: 46,
-      duration: 0.16666666,
-      route: "rome",
-      description: "Roam the eternal city",
-      thumbnailPath:
-        "https://hoianexpress.com.vn/wp-content/uploads/2019/12/1-25-870x555.jpg",
-    },
-    {
-      id: 4,
-      tourName: "HOI AN COUNTRYSIDE ADVENTURE BY ELECTRIC SCOOTER",
-      reviews: 10,
-      rating: 4.2,
-      viewCount: 10,
-      isPrivate: false,
-      minPrice: 62,
-      duration: 0.5,
-      route: "paris",
-      description: "C'est La Vie",
-      thumbnailPath:
-        "https://hoianexpress.com.vn/wp-content/uploads/2019/12/1-Briefing-Hoi-An-Town-6-870x555.jpg",
-    },
-    {
-      id: 5,
-      tourName: "Private Tour: FULL-DAY GOLF TOUR FROM HA NOI",
-      reviews: 10,
-      rating: 4.6,
-      viewCount: 10,
-      isPrivate: true,
-      minPrice: 100,
-      duration: 0.33333333,
-      route: "new-york",
-      description: "Take a bite of the Big Apple",
-      thumbnailPath:
-        "https://hoianexpress.com.vn/wp-content/uploads/2019/12/GOLF_6-680x500.jpg",
-    },
-    {
-      id: 6,
-      tourName: "13 DAYS AUTHENTIC VIETNAM PLUS GOLDEN BRIDGE",
-      reviews: 10,
-      rating: 4.6,
-      viewCount: 10,
-      isPrivate: false,
-      minPrice: 200,
-      duration: 13,
-      thumbnailPath:
-        "https://hoianexpress.com.vn/wp-content/uploads/2019/12/GBS0056_13days9-680x500.jpg",
-    },
-    {
-      id: 7,
-      tourName:
-        "HALF-DAY FOODIE TOUR BY BICYCLE & VISIT TRA QUE VEGETABLE VILLAGE",
-      reviews: 10,
-      rating: 4.4,
-      viewCount: 10,
-      isPrivate: true,
-      minPrice: 32,
-      duration: 0.5,
-      thumbnailPath:
-        "https://hoianexpress.com.vn/wp-content/uploads/2019/12/Hoi-An-Food-tour-by-bike-_7-1-680x500.jpg",
-    },
-    {
-      id: 8,
-      tourName: "HALF-DAY DA NANG MUSEUMS AND BRIDGES TOUR",
-      reviews: 10,
-      rating: 4.8,
-      viewCount: 10,
-      isPrivate: false,
-      minPrice: 35,
-      duration: 0.5,
-      thumbnailPath:
-        "https://hoianexpress.com.vn/wp-content/uploads/2019/12/HCM-MUSEUM_49-680x500.jpg",
-    },
-    {
-      id: 9,
-      tourName: "FULL-DAY DMZ TOUR FROM HUE CITY",
-      reviews: 10,
-      rating: 4.8,
-      viewCount: 10,
-      isPrivate: false,
-      minPrice: 89,
-      duration: 1,
-      thumbnailPath:
-        "https://hoianexpress.com.vn/wp-content/uploads/2021/06/1-680x500.jpg",
-    },
-    {
-      id: 10,
-      tourName: "BA NA HILLS AND GOLDEN BRIDGE FROM CHAN MAY PORT",
-      reviews: 10,
-      rating: 4.2,
-      viewCount: 10,
-      isPrivate: false,
-      minPrice: 100,
-      duration: 0.29166666,
-      thumbnailPath:
-        "https://hoianexpress.com.vn/wp-content/uploads/2019/12/BA-NA-HILLS_44-1-680x500.jpg",
-    },
-    {
-      id: 11,
-      tourName: "Private Tour: HUE HERITAGE",
-      reviews: 10,
-      rating: 3.8,
-      viewCount: 10,
-      isPrivate: true,
-      minPrice: 1.043,
-      duration: 2,
-      thumbnailPath:
-        "https://hoianexpress.com.vn/wp-content/uploads/2019/12/HUE-CITY-_6-680x500.jpg",
-    },
-    {
-      id: 12,
-      tourName: "FOOD TOUR IN HUE CITY",
-      reviews: 10,
-      rating: 4.4,
-      viewCount: 10,
-      isPrivate: true,
-      minPrice: 162,
-      duration: 0.16666666,
-      thumbnailPath:
-        "https://hoianexpress.com.vn/wp-content/uploads/2021/08/Food-Tour-Hue_4-680x500.jpg",
-    },
-    {
-      id: 13,
-      tourName: "FOOD TOUR IN HUE CITY",
-      reviews: 10,
-      rating: 4.8,
-      viewCount: 10,
-      isPrivate: false,
-      minPrice: 63,
-      duration: 2,
-      thumbnailPath:
-        "https://hoianexpress.com.vn/wp-content/uploads/2021/06/dnp-Top-10-mon-an-vat-ngon-nhat-o-hue-banh-be%CC%80o-870x555.jpeg",
-    },
-    {
-      id: 14,
-      tourName: "HOIANIAN’S BELIEFS AND VEGETARIAN DINNER FROM HOI AN",
-      reviews: 10,
-      rating: 3.8,
-      viewCount: 10,
-      isPrivate: true,
-      minPrice: 46,
-      duration: 0.16666666,
-      thumbnailPath:
-        "https://cdn-imgix.headout.com/cities/edinburgh/images/mobile/morning.jpg?auto=compress&fm=webp&w=412.5&h=486&crop=faces&fit=min",
-    },
-    {
-      id: 15,
-      tourName: "FULL-DAY HUE CITY TOUR & CRAFT VILLAGES",
-      reviews: 10,
-      rating: 4.8,
-      viewCount: 10,
-      isPrivate: false,
-      minPrice: 79,
-      duration: 2,
-      thumbnailPath:
-        "https://hoianexpress.com.vn/wp-content/uploads/2021/06/Screen-Shot-2021-05-23-at-08.47.26-680x500.png",
-    },
-    {
-      id: 16,
-      tourName: "Private Tour: HALF-DAY COUNTRYSIDE BY BICYCLE FROM HUE CITY",
-      reviews: 10,
-      rating: 4.8,
-      viewCount: 10,
-      isPrivate: true,
-      minPrice: 174,
-      duration: 0.16666666,
-      thumbnailPath:
-        "https://hoianexpress.com.vn/wp-content/uploads/2021/08/Food-Tour-Hue_5-680x500.jpg",
-    },
-    {
-      id: 17,
-      tourName: "Private Tour: FULL-DAY HUE CITY TOUR & CRAFT VILLAGES",
-      reviews: 10,
-      rating: 4.8,
-      viewCount: 10,
-      isPrivate: true,
-      minPrice: 307,
-      duration: 1,
-      route: "florence",
-      description: "Enjoy and have fun in the City",
-      thumbnailPath:
-        "https://hoianexpress.com.vn/wp-content/uploads/2021/08/Hue_18-680x500.jpg",
-    },
-    {
-      id: 18,
-      tourName: "Private Tour: THREE-DAY DA LAT FLOWER & WATERFALL CITY",
-      reviews: 10,
-      rating: 4.8,
-      viewCount: 10,
-      isPrivate: true,
-      minPrice: 1.223,
-      duration: 3,
-      route: "florence",
-      description: "Enjoy and have fun in the City",
-      thumbnailPath:
-        "https://hoianexpress.com.vn/wp-content/uploads/2019/12/hinh-anh-da-lat-5-680x500.jpg",
-    },
-];
 
 const mapStateToProps = (state) => {
     return {
